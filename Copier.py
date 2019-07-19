402f1bc531d631d723663e91ded066025c81e0a4
Bereket Guta <bereket.guta@PELESYS.NET>
2019-07-19 10:25:34 -0700

    Refactored the code to make it slighly more efficient

--- Copier.py
+++ Copier.py
@@ -5,8 +5,8 @@ def generate(files, cwd):
     #------------------------------------------------------------------------------------------#
     # words to replace, make sure that each element in orignal corresponds to the same element in new
     # order is important, make sure to have the plural form first and the singular afterwards.
-    orignal = ["Organizations", "organizations", "Organization", "organization"] # You can modify this fiels
-    new     = ["Contracts", "contracts", "Contract", "contract"] # You can modify this fiels
+    orignal = ["Organizations", "organizations", "Organization", "organization"] # You can modify these entries
+    new     = ["Contracts", "contracts", "Contract", "contract"] # You can modify these entries
     #------------------------------------------------------------------------------------------#
     
     mapping = {x:y for x,y in zip(orignal, new)}
@@ -18,17 +18,22 @@ def generate(files, cwd):
     if not os.path.exists(newCWD):
         os.makedirs(newCWD)
 
-    # replace the title, make sure it is the capitalized one
-    for arg in files:
-        newFName = arg.replace(replacements[0][0], replacements[0][1])
-        newFName = newFName.replace(replacements[2][0], replacements[2][1])
-        # print("Replacing " + arg + " with " + newFName)
-        with open(cwd + arg, "rt") as inputF:
-            with open(newCWD + newFName, "wt") as outputF:
-                for line in inputF:
-                    for rp in replacements:
-                        line = line.replace(rp[0], rp[1])
-                    outputF.write(line)
+    def reproduce(arg):
+        # avoid replacing these two files
+        if not (arg == 'Copier.py' or arg == 'README.md'):
+            # replace the title, make sure it is the capitalized one
+            newFName = arg.replace(replacements[0][0], replacements[0][1])
+            newFName = newFName.replace(replacements[2][0], replacements[2][1])
+
+            with open(cwd + arg, "rt") as inputF:
+                with open(newCWD + newFName, "wt") as outputF:
+                    for line in inputF:
+                        for rp in replacements:
+                            line = line.replace(rp[0], rp[1])
+                        outputF.write(line)
+
+    return list(map(lambda x: reproduce(x), files))
+
 def replace(path):
     cwd = os.path.join(path, '')
     entries = os.listdir(cwd)
